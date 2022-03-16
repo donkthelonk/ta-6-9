0dced6d327fd406900844a6633804f2092d64779
Robert Giles <robert.ryan.giles@gmail.com>
2022-03-15 21:14:00 -0500

    Updated code to end of video 7

--- Video_6/Program.vb
+++ Video_6/Program.vb
@@ -2,6 +2,9 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        'This line writes Hello World! to the console
         Console.WriteLine("Hello World!")
+        'This line pauses so the text can be read
+        Console.ReadLine()
     End Sub
 End Module
